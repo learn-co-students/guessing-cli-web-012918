0888efa29b5deb6774fdaced6cdeb5f477e726e3
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2018-01-20 03:12:25 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -15,20 +15,3 @@ def run_guessing_game1
   end
 
 end
-
-def run_guessing_game
-  puts "Guess a number between 1 and 6."
-  response = gets.chomp
-
-  while response != "exit" do
-    rand = 1 + rand(6)
-    if response == rand
-      puts "You guessed the correct number!"
-      break
-    else
-      puts "The computer guessed #{rand}."
-      break
-    end
-  end
-  puts "Goodbye!"
-end
